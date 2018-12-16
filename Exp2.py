f7c7ac2181e9eeab6c162c8b67bbf4724735cf33
sfarrugia15 <31629421+sfarrugia15@users.noreply.github.com>
2018-12-16 19:34:32 +0100

    Facenet

--- Exp2.py
+++ Exp2.py
@@ -11,10 +11,11 @@ import os
 import time
 import cv2
 #from resnet50 import ResNet50
-from vgg16 import VGG16
+#from vgg16 import VGG16
+from delorean import now
 from keras.applications.resnet50 import ResNet50
-#from keras.applications.vgg16 import VGG16
-
+from keras.applications.vgg16 import VGG16
+from keras.models import model_from_json
 from keras.preprocessing import image as Image
 from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Activation, Flatten
 from keras.layers import Input
@@ -29,17 +30,17 @@ from sklearn import preprocessing
 from keras import optimizers
 from keras.engine import Model
 from keras.layers import Flatten, Dense, Input
-from keras_vggface.vggface import VGGFace
+#from keras_vggface.vggface import VGGFace
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.preprocessing import MultiLabelBinarizer
 from keras.applications.imagenet_utils import preprocess_input
 import matplotlib
+from tensorflow.python.keras.models import load_model
+
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 #import matplotlib.pyplot as plt
 #from imagenet_utils import decode_predictions
-
-
 
 
 nb_classes = 136
@@ -47,12 +48,10 @@ nb_epochs = 10
 b_size = 16
 augment_data = False
 PATH = os.getcwd()
-data_path = PATH + '/Dataset'
+data_path = PATH + '/Dataset_resized'
 data_dir_list = os.listdir(data_path)
 plot_data = False
-training_model = 3
-
-mlb = MultiLabelBinarizer()
+training_model = 4
 
 img_data_list = []
 labels = []
@@ -61,27 +60,19 @@ label_test = []
 
 for folder_name in data_dir_list:
 	img_list = os.listdir(data_path + '/' + folder_name)
-
 	for image in img_list:
 		retrieve_dir = data_path + "/" + folder_name + "/" + image
 		images = cv2.imread(retrieve_dir, 3)
 		list_of_image_paths.append(images)
-
 	img_data_list.append(img_list)
 	labels.append(folder_name)
-
 	label_test.append([folder_name] * len(img_list))
 
-
 list_of_image_paths = np.array(list_of_image_paths)
-flattened_list = [y for x in label_test for y in x]
-flattened_list = np.asarray(flattened_list, dtype="str")
-lb = preprocessing.LabelBinarizer()
-lb.fit(flattened_list)
-lb.classes_
+flattened_list = np.asarray([y for x in label_test for y in x], dtype="str")
+lb = preprocessing.LabelBinarizer().fit(flattened_list)
 labels = lb.transform(flattened_list)
-#labels = mlb.fit_transform(flatten_list)
-print(len(labels))
+print("Number of Labels: ", len(labels))
 
 X_train, X_test, y_train, y_test = train_test_split(list_of_image_paths, labels, test_size=0.2, random_state=2)
 
@@ -89,7 +80,7 @@ augmented_Xtrain = []
 y_train_labels = []
 
 images = []
-train_datagen = ImageDataGenerator(featurewise_std_normalization=True, rotation_range=10, horizontal_flip=True, vertical_flip=True, zoom_range=0.4)
+train_datagen = ImageDataGenerator(rotation_range=10, horizontal_flip=True, vertical_flip=True, zoom_range=0.4)
 
 for index in range(len(X_train)):
 
@@ -113,62 +104,62 @@ im_shape = (224,224,3)
 image_input = Input(shape=im_shape)
 
 #################VGGFace Model############
-if training_model == 1:
-
-	hidden_dim = 2048
-
-	vggface_model = VGGFace(input_tensor=image_input, include_top=True, weights='vggface', input_shape=im_shape,
-		                     pooling='max')
-	vggface_model.summary()
-
-	pool5 = vggface_model.layers[-8]
-
-	flatten = vggface_model.layers[-7]
-
-	fc6 = vggface_model.layers[-6]
-	fc6relu = vggface_model.layers[-5]
-
-	fc7 = vggface_model.layers[-4]
-	fc7relu = vggface_model.layers[-3]
-
-	fc8 = vggface_model.layers[-2]
-	fc8relu = vggface_model.layers[-1]
-
-	dropout1 = Dropout(0.5)
-	dropout2 = Dropout(0.5)
-
-	# Reconnect the layers
-	x = dropout1(pool5.output)
-	x = flatten(x)
-	x = fc6(x)
-	x = fc6relu(x)
-	x = dropout2(x)
-	x = fc7(x)
-	x = fc7relu(x)
-	out = Dense(CLASSESNR, activation='softmax', name='output')(x)
-
-	custom_vggface_model = Model(vggface_model.input, output=out)
-	 # Resnet_model = ResNet50(weights='imagenet',include_top=False)
-
-	vggface_model.summary()
-	custom_vggface_model.summary()
-
-	for layer in custom_vggface_model.layers[:-1]:
-		 layer.trainable = False
-
-	custom_vggface_model.layers[-1].trainable
-	custom_vggface_model.summary()
-
-	sgd = optimizers.SGD(lr=0.001, decay=1e-5, momentum=0.9, nesterov=True)
-	custom_vggface_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-	t = time.time()
-	hist = custom_vggface_model.fit(augmented_X_train, y_train_labels, batch_size= b_size, epochs=nb_epochs, verbose=1,
-		                             validation_data=(X_test, y_test))
-	print('Training time: %s' % (t - time.time()))
-	(loss, accuracy) = custom_vggface_model.evaluate(X_test, y_test, batch_size= b_size, verbose=1)
-
-	print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss, accuracy * 100))
+# if training_model == 1:
+#
+# 	hidden_dim = 2048
+#
+# 	vggface_model = VGGFace(input_tensor=image_input, include_top=True, weights='vggface', input_shape=im_shape,
+# 		                     pooling='max')
+# 	vggface_model.summary()
+#
+# 	pool5 = vggface_model.layers[-8]
+#
+# 	flatten = vggface_model.layers[-7]
+#
+# 	fc6 = vggface_model.layers[-6]
+# 	fc6relu = vggface_model.layers[-5]
+#
+# 	fc7 = vggface_model.layers[-4]
+# 	fc7relu = vggface_model.layers[-3]
+#
+# 	fc8 = vggface_model.layers[-2]
+# 	fc8relu = vggface_model.layers[-1]
+#
+# 	dropout1 = Dropout(0.5)
+# 	dropout2 = Dropout(0.5)
+#
+# 	# Reconnect the layers
+# 	x = dropout1(pool5.output)
+# 	x = flatten(x)
+# 	x = fc6(x)
+# 	x = fc6relu(x)
+# 	x = dropout2(x)
+# 	x = fc7(x)
+# 	x = fc7relu(x)
+# 	out = Dense(nb_classes, activation='softmax', name='output')(x)
+#
+# 	custom_vggface_model = Model(vggface_model.input, output=out)
+# 	 # Resnet_model = ResNet50(weights='imagenet',include_top=False)
+#
+# 	vggface_model.summary()
+# 	custom_vggface_model.summary()
+#
+# 	for layer in custom_vggface_model.layers[:-1]:
+# 		 layer.trainable = False
+#
+# 	custom_vggface_model.layers[-1].trainable
+# 	custom_vggface_model.summary()
+#
+# 	sgd = optimizers.SGD(lr=0.001, decay=1e-5, momentum=0.9, nesterov=True)
+# 	custom_vggface_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+#
+# 	t = time.time()
+# 	hist = custom_vggface_model.fit(augmented_X_train, y_train_labels, batch_size= b_size, epochs=nb_epochs, verbose=1,
+# 		                             validation_data=(X_test, y_test))
+# 	print('Training time: %s' % (t - time.time()))
+# 	(loss, accuracy) = custom_vggface_model.evaluate(X_test, y_test, batch_size= b_size, verbose=1)
+#
+# 	print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss, accuracy * 100))
 
 #################VGG Model############
 if training_model == 2:
@@ -196,7 +187,7 @@ if training_model == 2:
 
 	print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
 
-
+#####RESNET 50#######
 if training_model == 3:
 
 	model = ResNet50(input_tensor=image_input, include_top=True,weights='imagenet')
@@ -214,11 +205,10 @@ if training_model == 3:
 	sgd = optimizers.SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
 	custom_resnet_model.compile(loss='binary_crossentropy',optimizer=sgd,metrics=['accuracy', 'binary_crossentropy'])
 
-
 	t=time.time()
 	hist = custom_resnet_model.fit(augmented_X_train, y_train_labels, batch_size=b_size, epochs=nb_epochs, verbose=2, validation_data=(X_test, y_test))
 	print('Training time: %s' % (t - time.time()))
-	#(loss, accuracy) = custom_resnet_model.evaluate(X_test, y_test, batch_size=32, verbose=1)
+	(loss, accuracy) = custom_resnet_model.evaluate(X_test, y_test, batch_size=32, verbose=1)
 
 	#print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,accuracy * 100))
 
@@ -233,8 +223,12 @@ if training_model == 3:
 		print(index,index2, prediction[0][index], test_label[index], prediction[0][index2])
 	
 	if plot_data:
-		polot_daa(hist)
+		plot_data(hist)
 
+if training_model == 4:
+	import facenet_recognition
+
+	facenet_recognition.align_input('input_images', 'aligned_images')
 
 
 def plot_data(hist):
